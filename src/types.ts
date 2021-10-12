67cad3897585ae9788a8a514354f13fa732c0f6a
runimo <63205791+runimo@users.noreply.github.com>
2021-10-12 21:37:59 +0200

    feat: add custom eventItem type

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+// Types used in this app
+
+
+export interface EventItem {
+  "id": number;
+  "category": string;
+  "title": string;
+  "description": string;
+  "location": string;
+  "date": string;
+  "time": string;
+  "organizer": string;
+}
