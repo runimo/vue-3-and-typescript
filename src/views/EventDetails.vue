ce2405356778ab6a3a856b4cfb7fc778233e0c84
runimo <63205791+runimo@users.noreply.github.com>
2021-10-12 21:29:39 +0200

    fix: use relative file path

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import EventService from '@/services/EventService'
+import EventService from '../services/EventService'
 export default defineComponent ({
   props: ['id'],
   data() {
